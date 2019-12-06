8c7a1a9d1b6556819f0a4f3763bbb2e03a4ced66
michael-doll <MWDoll88@gmail.com>
2019-12-06 13:43:09 -0500

    save work, muffinController test

--- src/main/java/com/zipcodewilmington/bakery/controllers/MuffinController.java
+++ src/main/java/com/zipcodewilmington/bakery/controllers/MuffinController.java
@@ -17,27 +17,27 @@ public class MuffinController {
         this.service = service;
     }
 
-    @GetMapping("/Muffin")
+    @GetMapping("/muffins")
     public ResponseEntity<Iterable<Muffin>> index() {
         return new ResponseEntity<>(service.index(), HttpStatus.OK);
     }
 
-    @RequestMapping("/Muffin/{id}")
+    @RequestMapping("/muffins/{id}")
     public ResponseEntity<Muffin> show(@PathVariable Long id) {
         return new ResponseEntity<>(service.show(id), HttpStatus.OK);
     }
 
-    @PostMapping("/Muffin")
+    @PostMapping("/muffins")
     public ResponseEntity<Muffin> create(@RequestBody Muffin baker) {
         return new ResponseEntity<>(service.create(baker), HttpStatus.CREATED);
     }
 
-    @PutMapping("/Muffin/{id}")
-    public ResponseEntity<Muffin> update(Long id, Muffin baker) {
+    @PutMapping("/muffins/{id}")
+    public ResponseEntity<Muffin> update(@PathVariable Long id,@RequestBody Muffin baker) {
         return new ResponseEntity<>(service.update(id, baker), HttpStatus.OK);
     }
 
-    @DeleteMapping("/muffin/{id}")
+    @DeleteMapping("/muffins/{id}")
     public ResponseEntity<Boolean> destroy(@PathVariable Long id) {
         return new ResponseEntity<>(service.delete(id), HttpStatus.OK);
     }
